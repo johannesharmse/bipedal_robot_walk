48791b97d895186c41e368e57c12ff15bbe0174d
johannesharmse <johannesharmse91@gmail.com>
2018-10-29 13:03:02 -0700

    Remove redundant

--- src/bipedal.py
+++ src/bipedal.py
@@ -4,38 +4,6 @@ import numpy as np
 import os
 
 
-class Agent():
-    def __init__(self):
-        
-
-   
-
-
-class Hp():
-    # Hyperparameters
-    def __init__(self,
-                 nb_steps=1000,
-                 episode_length=2000,
-                 learning_rate=0.02,
-                 num_deltas=16,
-                 num_best_deltas=16,
-                 noise=0.03,
-                 seed=1,
-                 env_name='BipedalWalker-v2',
-                 record_every=50):
-
-        self.nb_steps = nb_steps
-        self.episode_length = episode_length
-        self.learning_rate = learning_rate
-        self.num_deltas = num_deltas
-        self.num_best_deltas = num_best_deltas
-        assert self.num_best_deltas <= self.num_deltas
-        self.noise = noise
-        self.seed = seed
-        self.env_name = env_name
-        self.record_every = record_every
-
-
 class Normalizer():
     # Normalizes the inputs
     def __init__(self, n_inputs):
