8d1adc0aca5db7806b9a7ad9e29aba09fffbb74b
johannesharmse <johannesharmse91@gmail.com>
2018-10-29 10:37:54 -0700

    Create env

--- src/bipedal.py
+++ src/bipedal.py
@@ -1,4 +1,35 @@
+import gym
 import os
+
+
+class Agent():
+    def __init__(self):
+        self.env = gym.make()
+
+    def env(self, env):
+        """Define Gym Environment
+        
+        Args:
+            env(str): OpenAI Gym environment name.
+        """
+        
+        self.env = gym.make(ENV_NAME)
+        
+
+    def fit(self, n_steps, episode_length, lr, 
+    n_deltas, n_best_deltas, noise, 
+    seed, env_name, log_freq):
+    """Fit hyperparameters for agent training.
+
+    Args:
+        n_steps (int): Number of steps per 
+
+
+
+    """
+
+
+
 
 if __name__ == '__main__':
     # openAI gym environment
